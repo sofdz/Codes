cead04eff296b91fb9347e2077f7e7d288e1b505
sofdz <sofdzed@gmail.com>
2023-08-05 18:54:04 +0100

    secure

--- backEnd/db/routers.js/user.js
+++ backEnd/db/routers.js/user.js
@@ -31,7 +31,7 @@ UserRouter.post('/login',passport.authenticate('local',{session:false}),async (r
    const {_id,nom_utilisateur,role,nom,prenom}=req.user
    const token=jwt.sign({sub:_id,role},'salah_hafnaoui_secret_key',{expiresIn:'10h'})
    console.log('in')
-   res.cookie('access_token',token,{sameSite:'none',httpOnly:true})
+   res.cookie('access_token',token,{sameSite:'none',httpOnly:true,secure:true})
    res.status(200).json({estAuthentifie:true,utilisateur:{nom_utilisateur,role,nom,prenom}})
  }
    }
