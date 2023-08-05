bb79974cdb301ccd056f46791500644838eb94ba
sofdz <sofdzed@gmail.com>
2023-08-05 19:22:47 +0100

    front cookies

--- Memoire/memoire/src/components/utilisatuers/Utilisateurs.js
+++ Memoire/memoire/src/components/utilisatuers/Utilisateurs.js
@@ -1,6 +1,6 @@
 import { DataGrid } from '@mui/x-data-grid'
 import React, { useEffect, useState } from 'react'
-import { Grid,Typography,Button } from '@mui/material'
+import { Grid, Typography, Button } from '@mui/material'
 import Adduser from './Adduser'
 import AddIcon from '@mui/icons-material/Add'
 import DeleteIcon from '@mui/icons-material/Delete'
@@ -9,76 +9,80 @@ import ModifierUser from './ModiferUser'
 import Deletee from './Delete'
 
 function Utilisateurs() {
-    const [users,setusers]=useState(null)
-    const [openAdd,setOpenAdd]=useState(false)
-    const [openUpdate,setopenUpdate]=useState(false)
-    const [selected,setSelected]=useState([])
-    const columns = [
-        {
-          field: 'nom',
-          headerName: 'Nom',
-          editable: true,
-          flex:1
-        },
-        {
-          field: 'prenom',
-          headerName: 'Prénom',
-          editable: true,
-          flex:1
-        },
-        {
-            field: 'nom_utilisateur',
-            headerName: 'Nom utilisateur',
-            editable: true,
-            flex:1
-          }
-        ,
-        {
-            field: 'telephone',
-            headerName: 'Téléphone',
-            sortable: false,
-            flex:1
-          }, {
-            field: 'role',
-            headerName: 'Role',
-            sortable: false,
-            flex:1
-          },]
-    useEffect(()=>{
-    fetch('https://ophtaback.onrender.com/userss').then(res=>{res.json().then(data=>{setusers(data)})})
-    },[])
+  const [users, setusers] = useState(null)
+  const [openAdd, setOpenAdd] = useState(false)
+  const [openUpdate, setopenUpdate] = useState(false)
+  const [selected, setSelected] = useState([])
+  const columns = [
+    {
+      field: 'nom',
+      headerName: 'Nom',
+      editable: true,
+      flex: 1
+    },
+    {
+      field: 'prenom',
+      headerName: 'Prénom',
+      editable: true,
+      flex: 1
+    },
+    {
+      field: 'nom_utilisateur',
+      headerName: 'Nom utilisateur',
+      editable: true,
+      flex: 1
+    }
+    ,
+    {
+      field: 'telephone',
+      headerName: 'Téléphone',
+      sortable: false,
+      flex: 1
+    }, {
+      field: 'role',
+      headerName: 'Role',
+      sortable: false,
+      flex: 1
+    },]
+  useEffect(() => {
+    fetch('https://ophtaback.onrender.com/userss',
+      {
+        method: 'GET',
+        credentials: 'include',
+      }).then(res => { res.json().then(data => { setusers(data) }) })
+  }, [])
   return (
     <>
-<Typography variant="h3" color='primary'>Utilisateurs</Typography>
-    {users&&
-    <Grid container direction='column' justifyContent='center' alignContent='center' alignItems='center'>
-            <Adduser open={openAdd} setopen={setOpenAdd} usernames={users.map(user=>user.nom_utilisateur)}/>
-            {openUpdate&&<ModifierUser setopen={setopenUpdate} userM={users.find(user=>user._id==selected)} usernames={users.map(user=>{if(user._id!=selected){return(user.nom_utilisateur)}})}/>}
+      <Typography variant="h3" color='primary'>Utilisateurs</Typography>
+      {users &&
+        <Grid container direction='column' justifyContent='center' alignContent='center' alignItems='center'>
+          <Adduser open={openAdd} setopen={setOpenAdd} usernames={users.map(user => user.nom_utilisateur)} />
+          {openUpdate && <ModifierUser setopen={setopenUpdate} userM={users.find(user => user._id == selected)} usernames={users.map(user => { if (user._id != selected) { return (user.nom_utilisateur) } })} />}
           <Grid container item direction='row' justifyContent="center">
-             <Grid item> <Button onClick={() => setOpenAdd(true)}> <AddIcon /></Button></Grid>
-             {selected.length==1&&<Grid item><Button onClick={() => {setopenUpdate(true)}}> <EditTwoToneIcon /></Button></Grid>}
-          </Grid> 
-    <Grid item width='90%'>
-    <DataGrid
-    rows={users}
-    columns={columns}
-    getRowId={(row) => row._id}
-    responsiveLayout
-    initialState={{
-      pagination: {
-        paginationModel: {
-          pageSize: 15,
-        },
-      },
-    }}
-    pageSizeOptions={[5]}
-    checkboxSelection
-    disableRowSelectionOnClick
-    onRowSelectionModelChange={(items) => { setSelected(items) }}
-    hideFooter
-    />
-    </Grid>
-    </Grid>}</>
+            <Grid item> <Button onClick={() => setOpenAdd(true)}> <AddIcon /></Button></Grid>
+            {selected.length == 1 && <Grid item><Button onClick={() => { setopenUpdate(true) }}> <EditTwoToneIcon /></Button></Grid>}
+          </Grid>
+          <Grid item width='90%'>
+            <DataGrid
+              rows={users}
+              columns={columns}
+              getRowId={(row) => row._id}
+              responsiveLayout
+              initialState={{
+                pagination: {
+                  paginationModel: {
+                    pageSize: 15,
+                  },
+                },
+              }}
+              pageSizeOptions={[5]}
+              checkboxSelection
+              disableRowSelectionOnClick
+              onRowSelectionModelChange={(items) => { setSelected(items) }}
+              hideFooter
+            />
+          </Grid>
+        </Grid>}</>
   )
 }
 
