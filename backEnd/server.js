ba52e849c178080978aeb623d1728a822a33ef11
sofdz <sofdzed@gmail.com>
2023-08-05 05:16:07 +0100

    FF

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express=require('express')
 const fileDattenteRoute = require('./db/routers.js/fileDattente');
 const rendezVousRoute = require('./db/routers.js/rendezVousRouter');
 const PatientRouter = require('./db/routers.js/patients');
@@ -6,7 +6,7 @@ const MedicamentRouter = require('./db/routers.js/MedicamentRouter');
 const OrdonnanceRouter = require('./db/routers.js/OrdonnanceRouter');
 const UserRouter = require('./db/routers.js/user');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const cors=require('cors');
 const ActeRouter = require('./db/routers.js/ActeRouter');
 const ConsultationRouter = require('./db/routers.js/ConsultRouter');
 const TemplateRouter = require('./db/routers.js/TemplateRouter');
@@ -16,25 +16,23 @@ const User = require('./db/models/users');
 const Ordonnance = require('./db/models/OrdonnanceSc');
 const formidable = require('formidable')
 const mongoose = require('mongoose')
-const app = express()
+const app=express()
 app.use(
     cors({
-        origin: "*",
-        methods: ["POST", "GET", "PUT", "DELETE"],
-        credentials: true,
-        preflightContinue: false,
-        optionsSuccessStatus: 204
+        origin: "https://ophta-4f4blut6p-sofdz.vercel.app",
+        methods:["POST","GET","PUT","DELETE","PATCH"],
+        credentials: true
     })
-);
-/*app.use(function(req, res, next) {
-    res.header('Content-Type', 'application/json;charset=UTF-8')
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    next()
-  })*/
+    ); 
+    /*app.use(function(req, res, next) {
+        res.header('Content-Type', 'application/json;charset=UTF-8')
+        res.header('Access-Control-Allow-Credentials', true)
+        res.header(
+          'Access-Control-Allow-Headers',
+          'Origin, X-Requested-With, Content-Type, Accept'
+        )
+        next()
+      })*/
 
 //app.use(express.json())
 /*app.use(function (req, res, next) {
@@ -55,7 +53,7 @@ app.use(
     // Pass to next layer of middleware
     next();
 })*/
-mongoose.connect('mongodb://sofdz:moi1999@ac-leoaajk-shard-00-00.3rm2rif.mongodb.net:27017,ac-leoaajk-shard-00-01.3rm2rif.mongodb.net:27017,ac-leoaajk-shard-00-02.3rm2rif.mongodb.net:27017/newdb?replicaSet=atlas-mc1sdx-shard-0&ssl=true&authSource=admin').then((e) => (console.log("connected to DB"))).catch(e => console.log('error :' + e))
+mongoose.connect('mongodb://sofdz:moi1999@ac-leoaajk-shard-00-00.3rm2rif.mongodb.net:27017,ac-leoaajk-shard-00-01.3rm2rif.mongodb.net:27017,ac-leoaajk-shard-00-02.3rm2rif.mongodb.net:27017/newdb?replicaSet=atlas-mc1sdx-shard-0&ssl=true&authSource=admin').then((e)=>(console.log("connected to DB"))).catch(e=>console.log('error :' +e))
 
 app.use(cookieParser())
 app.use(express.json())
@@ -75,8 +73,8 @@ app.use(RegRouter)
 
 const port = 8080 || process.env.PORT
 
-app.listen(port, error => {
-    if (error) {
+app.listen(port,error=>{ 
+    if(error){
         console.log(`the port 8000`)
     }
 })
